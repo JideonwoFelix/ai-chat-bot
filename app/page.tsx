0b80e1c1b62f51c35a011112ad154d500ea40648
JideonwoFelix <99179966+JideonwoFelix@users.noreply.github.com>
2024-01-09 05:24:52 +0100

    nsuk logo

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="bg-[#f4f5f7] min-h-[100vh]">
       <div className='landing'>
         <div className="heading bg-gradient-to-tl to-[#cfad1f] from-[#93a877] rounded-b-[50px]">
-          <div>Logo</div>
+          <div className='absolute top-10 left-10'><Image src={'/nsuk-logo.png'} height={20} width={70} alt='nsuk-logo'/></div>
           <div className="bg-white rounded-2xl shadow-lg md:w-2/3 mx-5 md:mx-auto translate-y-1/2">
             <h2 className='border-b p-7 font-bold text-[#535353]'>Chat with NSUK team</h2>
             <div className='flex p-7 items-center gap-10 pb-[50px]'>
@@ -18,7 +18,7 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="faq bg-white rounded-2xl md:w-2/3 mx-5 md:mx-auto mt-[150px] text-[#717171] text-2xl md:text-3xl font-bold">
+        <div className="faq bg-white bg-[url('/nsuk-logo.png')] bg-cover bg-blend-overlay bg-center bg-opacity-80 rounded-2xl md:w-2/3 mx-5 md:mx-auto mt-[150px] text-[#717171] text-2xl md:text-3xl font-bold">
           <div className="border-b p-10 text-[#535353] text-3xl flex justify-between items-center">
             FAQs
             <Image src={'/search-icon.png'} height={20} width={50} alt='search-icon'/>
